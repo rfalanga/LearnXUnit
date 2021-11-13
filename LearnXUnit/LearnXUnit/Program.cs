6e9fe27ae0436c1a18fc810aae31dd6d8208a884
Rod Falanga <rfalanga@users.noreply.github.com>
2021-11-13 11:29:22 -0700

    Mde Add2 a generic method. Doesn't build.

--- LearnXUnit/LearnXUnit/Program.cs
+++ LearnXUnit/LearnXUnit/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a = 2;
+int b = 3;
+int x = Add2<int>(a, b);
+
+T Add2<T>(T a, T b) => a + b 
