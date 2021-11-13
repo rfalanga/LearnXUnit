87ee47cf6abb1be3cff90153ba016fa3faef2aec
Rod Falanga <rfalanga@users.noreply.github.com>
2021-11-13 09:19:37 -0700

    added XUnit test project

---
+++ LearnXUnit/LearnXUnit.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LearnXUnit.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //TODO: Implement later
+        }
+    }
+}
