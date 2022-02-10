97927df1071ad3dc05e8f30854080914e545fc3d
Rod Falanga <rfalanga@users.noreply.github.com>
2022-02-09 17:40:44 -0700

    saving what I've got so far, for experimental purposes

--- LearnXUnit/LearnXUnit/Program.cs
+++ LearnXUnit/LearnXUnit/Program.cs
@@ -7,4 +7,11 @@ int a = 2;
 int b = 3;
 int x = Add2<int>(a, b);
 
-T Add2<T>(T a, T b) => a + b;
+//T Add2<T>(T a, T b) => a + b;
+T? Add2<T>(T a, T b)
+{
+    if (a is null || b is null)
+    {
+        return null;
+    }
+}
