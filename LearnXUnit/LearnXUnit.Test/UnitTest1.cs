2da4cc7af9a760f214989f1ba94f626b35056b08
Rod Falanga <rfalanga@users.noreply.github.com>
2022-02-09 18:39:58 -0700

    save what I've got so far

--- LearnXUnit/LearnXUnit.Test/UnitTest1.cs
+++ LearnXUnit/LearnXUnit.Test/UnitTest1.cs
@@ -8,7 +8,13 @@ namespace LearnXUnit.Test
         [Fact]
         public void Test1()
         {
-            Assert.Equal(1, 1);
+            //Assert.Equal(1, 1); //this worked fine - as you'd expect
+            //Arrange
+            var l = new LearnXUnit.Test
+
+            //Act
+
+            //Assert
         }
     }
 }
