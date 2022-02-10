73f60a6196d185268281dabd5bb008a24f0ca4c3
Rod Falanga <rfalanga@users.noreply.github.com>
2022-02-09 17:35:25 -0700

    updating comment

--- LearnXUnit/LearnXUnit/Program.cs
+++ LearnXUnit/LearnXUnit/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// The link actually isn't an example of using XUnit. I'm using the code in the link, to learn 
+// how to use XUnit. 
 Console.WriteLine("Hello, World!");
 
 int a = 2;
 int b = 3;
 int x = Add2<int>(a, b);
 
-T Add2<T>(T a, T b) => a + b 
+T Add2<T>(T a, T b) => a + b;
