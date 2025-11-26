5f38e4a9829d2d631ba8135bdf54f3b8cfd04a54
Rod Falanga <rfalanga@users.noreply.github.com>
2025-11-26 15:10:26 -0700

    Remove Test1 method and enable internal access for tests
    The `Test1` method in `UnitTest1.cs` was removed, including its
    associated `[Fact]` attribute and commented-out code. This method
    contained placeholders for Arrange, Act, and Assert steps, as well
    as exploratory comments about testing top-level statements.
    
    Updated `LearnXUnit.csproj` to include an `<InternalsVisibleTo>`
    element, allowing the `LearnXUnit.Test` assembly to access internal
    members of the `LearnXUnit` assembly. This change facilitates unit
    testing of internal methods or classes.

--- LearnXUnit/LearnXUnit.Test/UnitTest1.cs
+++ LearnXUnit/LearnXUnit.Test/UnitTest1.cs
@@ -7,21 +7,6 @@ namespace LearnXUnit.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
-            //Assert.Equal(1, 1); //this worked fine - as you'd expect
-            //Arrange
-            //var l = new LearnXUnit.Test
-            //var entryPoint = typeof(Program).Assembly.EntryPoint!;
-
-            // I've asked a question on SO for clarification: https://stackoverflow.com/questions/70645272/is-there-a-way-to-unit-test-top-level-statements-in-c
-
-            //Act
-
-            //Assert
-        }
-
-        [Fact]
         public void Test2()
         {
             // Arrange
